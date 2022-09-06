cc0f1d5084f70c34e2e84e6f956ebe07498538b3
Enrique Soria <me@enriquesoria.com>
2022-09-06 19:12:56 +0200

    fix: Fix preview_message

--- src/preview_message.py
+++ src/preview_message.py
@@ -1,11 +1,10 @@
 from github import Github
 
 from settings import GITHUB_REPO_NAME, GITHUB_TOKEN
+from src.main import get_pending_reviews_message
 
 if __name__ == "__main__":
     github = Github(GITHUB_TOKEN)
     repo = github.get_repo(GITHUB_REPO_NAME)
 
-    from main import GithubCog
-
-    print(GithubCog(None).get_pending_reviews_message(repo))
+    print(get_pending_reviews_message(repo))
