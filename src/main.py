66caf69d87b734fed7b19657ce51bd7566dff86a
Enrique Soria <me@enriquesoria.com>
2022-10-04 23:09:53 +0200

    chore: Use pycord instead of discord.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
-import asyncio
-
 import discord
-from discord.ext import commands
 from github import Github
 
 from github_repo import get_pending_review_pull_requests, pull_request_to_str
 from settings import ALLOWED_CHANNEL_IDS, DISCORD_TOKEN, GITHUB_REPO_NAME, GITHUB_TOKEN
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = discord.Bot()
 
 
 def channel_is_allowed(channel_id) -> bool:
@@ -32,15 +27,15 @@ def get_pending_reviews_message(repo):
     return f"{separator}\n**PRs with code review pending:**\n{separator}\n\n{msg}"
 
 
-@bot.command(name="pending_reviews")
+@bot.slash_command(name="pending_reviews")
 async def pending_reviews(ctx):
     if not channel_is_allowed(ctx.channel.id):
-        await ctx.channel.send(
+        await ctx.respond(
             f"Command not allowed in this channel (`id={ctx.channel.id}`)"
         )
         return
 
-    await ctx.channel.send(get_pending_reviews_message(repo))
+    await ctx.respond(get_pending_reviews_message(repo))
 
 
 @bot.command()
@@ -48,9 +43,8 @@ async def ping(ctx):
     await ctx.channel.send("pong!")
 
 
-async def main():
-    async with bot:
-        await bot.start(DISCORD_TOKEN)
+def main():
+    bot.run(DISCORD_TOKEN)
 
 
 if __name__ == "__main__":
@@ -61,5 +55,5 @@ if __name__ == "__main__":
 
     # Discord
     print("Running bot")
-    asyncio.run(main())
+    main()
     print("bot closed")
