a416f303e533a543468027fd8d428ee1f70bf56d
Enrique Soria <me@enriquesoria.com>
2025-10-06 15:57:44 +0200

    feat: Handle exceptions when parsing pull request urls

--- src/github_discord/cogs/utils.py
+++ src/github_discord/cogs/utils.py
@@ -11,7 +11,10 @@ def channel_is_allowed(channel_id) -> bool:
     return True
 
 
-def parse_pull_request_url(url: str):
-    parsed_url = urlparse(url)
-    _, user, repo_name, _, pr_number, *_ = parsed_url.path.split("/")
-    return f"{user}/{repo_name}", int(pr_number)
+def parse_pull_request_url(url: str) -> Tuple[str, int]:
+    try:
+        parsed_url = urlparse(url)
+        _, user, repo_name, _, pr_number, *_ = parsed_url.path.split("/")
+        return f"{user}/{repo_name}", int(pr_number)
+    except Exception as e:
+        raise ValueError(f"Invalid pull request URL: {url}") from e
